3d36616d9c01097e57e9c53b443a7057c5b9ff1e
trungdan <trungdang@silverlakeaxis.com>
2023-02-02 11:39:00 +0700

    config SpringIoC using XML

--- src/main/java/com/springioc/Application.java
+++ src/main/java/com/springioc/Application.java
@@ -2,13 +2,18 @@ package com.springioc;
 
 import com.springioc.bean.HelloWorld;
 import com.springioc.config.AppConfig;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        //AppConfig -> Quan ly Bean
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig -> Quan ly Bean
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // applicationContext -> Quan ly Bean
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloWorldBean");
         System.out.println(helloWorld.getMessage());
